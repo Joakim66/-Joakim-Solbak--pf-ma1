aab8a898d9eab4e0ee6f9bf99179ef24eef4b086
null null <Joakim.Solbak96@gmail.com>
2020-05-23 17:34:41 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+// question 1
+
+var firstVar;
+
+// question 2
+
+var name = "Joakim";
+
+// question 3
+
+var apples = 5;
+
+// question 4
+
+var division = 20/5;
+
+// question 5
+
+console.log(typeof "frog");
+
+// question 6
+
+var orderHasShipped = true;
+
+// question 7
+
+if(orderHasShipped === true) {
+    console.log("The order has shipped.");
+}
+else {
+    console.log("The order has not been shipped yet.");
+}
+
+// question 8
+
+var counter;
+for(counter = 0; counter < 10; counter++){
+  console.log(counter);
+}
